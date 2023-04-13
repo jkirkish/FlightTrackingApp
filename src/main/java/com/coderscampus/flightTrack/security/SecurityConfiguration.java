374cf8f65ac4bf617c91d5da088ff05b6d346f09
Joseph Kirkish <kirkish.2@gmail.com>
2023-04-13 17:47:33 -0400

    spring security and html files
    added a spring security package and html files

---
+++ src/main/java/com/coderscampus/flightTrack/security/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.coderscampus.flightTrack.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration extends WebSecurityConfiguration{
+	
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+
+}
