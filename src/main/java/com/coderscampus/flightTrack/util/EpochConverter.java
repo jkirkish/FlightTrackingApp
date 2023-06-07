157ce69b4a4ad4849a631d752946f71c7e2c4bc0
Joseph Kirkish <kirkish.2@gmail.com>
2023-06-06 22:33:15 -0400

    API Tests
    created a java file for tests on the open sky API requests.  Also created a utility file that converts EPOCH milliseconds to human readable formatted dates.

---
+++ src/main/java/com/coderscampus/flightTrack/util/EpochConverter.java
@@ -0,0 +1,43 @@
+package com.coderscampus.flightTrack.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EpochConverter {
+
+    // Convert epoch to human-readable date
+    public static String epochToHumanReadable(long epochSeconds) {
+        Date date = new Date(epochSeconds * 1000); // Convert seconds to milliseconds
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        return sdf.format(date);
+    }
+
+    // Convert human-readable date to epoch
+    public static long humanReadableToEpoch(String dateString) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        Date date = sdf.parse(dateString);
+        return date.getTime() / 1000; // Convert milliseconds to seconds
+    }
+
+    public static void main(String[] args) {
+        // Test epoch to human-readable conversion
+        long epochTime = 1686102929;
+        String humanReadableDate = epochToHumanReadable(epochTime);
+        System.out.println("Epoch to human-readable:");
+        System.out.println("Epoch: " + epochTime);
+        System.out.println("Human-readable date: " + humanReadableDate);
+
+        System.out.println();
+
+        // Test human-readable to epoch conversion
+        String dateString = "06-05-2023 12:22:09";
+        try {
+            long epoch = humanReadableToEpoch(dateString);
+            System.out.println("Human-readable to epoch:");
+            System.out.println("Human-readable date: " + dateString);
+            System.out.println("Epoch: " + epoch);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/com/coderscampus/flightTrack/OpenSkyIntegrationAPITest.java
+++ src/test/java/com/coderscampus/flightTrack/OpenSkyIntegrationAPITest.java
@@ -1,5 +1,6 @@
 package com.coderscampus.flightTrack;
 
+import java.io.IOException;
 import java.net.URI;
 
 import org.junit.Test;
@@ -7,8 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 //https://openskynetwork.github.io/opensky-api/rest.html
 public class OpenSkyIntegrationAPITest {
 	@Test
@@ -43,24 +44,51 @@ public class OpenSkyIntegrationAPITest {
 	    ResponseEntity<String>response = rt.getForEntity(uri, String.class);
 	    System.out.println(response);
 	}
-	@Test
-	public void openskyExampleAircraft() {
+	
+@Test
+public void openskyExampleArrivalAirport() throws IOException{
+		/*Retrieve flights for a certain airport which arrived within a given time interval [begin, end]. 
+		 * If no flights are found for the given period, HTTP stats 404 - Not found is returned with an 
+		 * empty response body.
+		 * The given time interval must not be larger than seven days!
+		 */
 		RestTemplate rt = new RestTemplate();
-	    URI uri = UriComponentsBuilder.fromHttpUrl("https://opensky-network.org/api/flights/aircraft")
-	    					 .queryParam("icao24","3c666b")
-	    					 .queryParam("begin","1685991864")
-	    					 .queryParam("end","1685999064")
+	    URI uri = UriComponentsBuilder.fromHttpUrl("https://opensky-network.org/api/flights/arrival")
+	    					 .queryParam("airport","kCMH")
+	    					 .queryParam("begin","1685898028")
+	    					 .queryParam("end","1685984428")
 	    					 .build()
 	    					 .toUri();
 	    
 	    
-	    ObjectMapper mapper = new ObjectMapper();
-	    try {
-	    	ResponseEntity<String>response = rt.getForEntity(uri, String.class);
-	    	System.out.println(mapper.writeValueAsString(response));
-	    }catch(JsonProcessingException e) {
-	      e.printStackTrace();
 	   
+	    	ResponseEntity<String>response = rt.getForEntity(uri, String.class);
+	    	ObjectMapper objectMapper = new ObjectMapper();
+	    	objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+	    	String formattedResponse = objectMapper.readValue(response.getBody(), Object.class).toString();
+	    	System.out.println(formattedResponse);
 	}
+@Test
+public void openskyExampleDepartureAirport() throws IOException{
+		/*Retrieve flights for a certain airport which departure within a given time interval [begin, end]. 
+		 * If no flights are found for the given period, HTTP stats 404 - Not found is returned with an 
+		 * empty response body.
+		 * The given time interval must not be larger than seven days!
+		 */
+		RestTemplate rt = new RestTemplate();
+	    URI uri = UriComponentsBuilder.fromHttpUrl("https://opensky-network.org/api/flights/departure")
+	    					 .queryParam("airport","kCMH")
+	    					 .queryParam("begin","1685898028")
+	    					 .queryParam("end","1685984428")
+	    					 .build()
+	    					 .toUri();
+	    
+	    
+	   
+	    	ResponseEntity<String>response = rt.getForEntity(uri, String.class);
+	    	ObjectMapper objectMapper = new ObjectMapper();
+	    	objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+	    	String formattedResponse = objectMapper.readValue(response.getBody(), Object.class).toString();
+	    	System.out.println(formattedResponse);
 	}
 }
