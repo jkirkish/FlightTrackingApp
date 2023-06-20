6eaa0423c246bba4e483a96a12eab837670e9fae
Joseph Kirkish <kirkish.2@gmail.com>
2023-06-20 17:34:51 -0400

    security
    added security files

---
+++ src/main/java/com/coderscampus/flightTrack/service/AdminService.java
@@ -0,0 +1,26 @@
+package com.coderscampus.flightTrack.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.flightTrack.domain.User;
+import com.coderscampus.flightTrack.repository.UserRepository;
+
+@Service
+public class AdminService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Secured({"ROLE_ADMIN","ROLE_SUPERUSER"})
+	public List<User>getAllUserAccounts(){
+		return userRepo.findAll();
+	}
+	
+	
+	
+
+}
--- src/main/java/com/coderscampus/flightTrack/web/DashBoardController.java
+++ src/main/java/com/coderscampus/flightTrack/web/DashBoardController.java
@@ -1,20 +1,25 @@
 package com.coderscampus.flightTrack.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.coderscampus.flightTrack.domain.User;
+import com.coderscampus.flightTrack.service.AdminService;
 
 @Controller
 public class DashBoardController {
 	
+	@Autowired
+	private AdminService adminService;
 	
 	@GetMapping("/dashboard")
 	  public String getDashboard (@AuthenticationPrincipal User user, ModelMap model) {
 		System.out.println(user);
 		model.put("user", user);
+		List<User> allUserAccounts = adminService.getAllUserAccounts();
 	    return "dashboard";
 	  }
 
