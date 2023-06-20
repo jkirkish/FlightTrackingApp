17a0500ae4c8c4677f71a63e7f18c02973f29f62
Joseph Kirkish <kirkish.2@gmail.com>
2023-06-20 13:31:10 -0400

    updated file
    updated files with dashboard controller and dashboard HTML

--- src/main/java/com/coderscampus/flightTrack/web/DashBoardController.java
+++ src/main/java/com/coderscampus/flightTrack/web/DashBoardController.java
@@ -1,14 +1,20 @@
 package com.coderscampus.flightTrack.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.coderscampus.flightTrack.domain.User;
 
 @Controller
 public class DashBoardController {
 	
 	
 	@GetMapping("/dashboard")
-	  public String getDashboard () {
+	  public String getDashboard (@AuthenticationPrincipal User user, ModelMap model) {
+		System.out.println(user);
+		model.put("user", user);
 	    return "dashboard";
 	  }
 
