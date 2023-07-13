256a3f4d30784811f6acdca386eeab2b2b8387b6
Joseph Kirkish <kirkish.2@gmail.com>
2023-07-12 23:09:25 -0400

    added javascript
    Added java Script validation checks to editSearch.html.   Tested the POM file to see if Java 11 or Spring boot 2.0 would work correctly with Spring Security.  More work is needed with Spring Security to match it up with Spring Boot and Java 17.

--- src/main/java/com/coderscampus/flightTrack/security/SecurityConfiguration.java
+++ src/main/java/com/coderscampus/flightTrack/security/SecurityConfiguration.java
@@ -1,18 +1,35 @@
-//package com.coderscampus.flightTrack.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//
-//public class SecurityConfiguration extends WebSecurityConfiguration{
-//	
-//	
-//	
-//}
+package com.coderscampus.flightTrack.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("trevor@craftycodr.com")
+                .password("password123")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .anyRequest().hasAnyRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard")
+                .permitAll();
+    }
+}
