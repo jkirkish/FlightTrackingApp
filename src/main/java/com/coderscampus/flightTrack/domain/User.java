8dda20c31d901854e41bed71ea344c348909f143
Joseph Kirkish <kirkish.2@gmail.com>
2023-07-21 00:19:54 -0400

    Overrides added
    override annotation added to getPassword and getUsername()

--- src/main/java/com/coderscampus/flightTrack/domain/User.java
+++ src/main/java/com/coderscampus/flightTrack/domain/User.java
@@ -74,12 +74,14 @@ public class User implements UserDetails{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	@Override
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	@Override
 	public String getPassword() {
 		return password;
 	}
